c24e88c77d96fc003ec6a8b06e9e2bdc40bf97e3
Arseniy <arseniypopugay@gmail.com>
2021-04-23 15:17:10 +0300

    B - Done

--- contest/B.cpp
+++ contest/B.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <deque>
-#include <sstream>
+#include <vector>
 
 struct numchar {
     char ch;
-    int position;
+    size_t position;
 };
 
 int main() {
@@ -12,33 +12,37 @@ int main() {
     std::deque<struct numchar> line = {{0}};
     char cur_char;
     bool is_capital;
-    int numerator = 1;
-    std::stringstream ss;
+    size_t animal_number = 1;
+    std::vector<size_t> cage_positioner;
 
     while (std::cin >> cur_char) {
         is_capital = cur_char - 'Z' <= 0;
 
 
         if (abs(cur_char - line.back().ch) == 32) {
-            if (is_capital) {
+            if (!is_capital) {
+                struct numchar last = line.back();
                 line.pop_back();
-                ss << numerator++ << " ";
+                cage_positioner[last.position] = animal_number++;
             } else {
                 struct numchar last = line.back();
                 line.pop_back();
-                ss << last.position << " ";
+                cage_positioner.push_back(last.position);
             }
         } else {
-            if (is_capital)
-                line.push_back({cur_char, numerator++});
-            else
-                line.push_back({cur_char, 0});
+            if (!is_capital) {
+                line.push_back({cur_char, animal_number++});
+            } else {
+                cage_positioner.push_back(0);
+                line.push_back({cur_char, cage_positioner.size()-1});
+            }
         }
     }
 
     if (line.size() == 1) {
         std::cout << "Possible" << std::endl;
-        std::cout << ss.str();
+        for (auto &el: cage_positioner)
+            std::cout << el << " ";
     } else
         std::cout << "Impossible";
 }
