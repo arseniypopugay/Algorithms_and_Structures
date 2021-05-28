14f832e85d09d073d16ec19dbe00b9c36b2d1fb8
Arseniy <arseniypopugay@gmail.com>
2021-05-28 18:47:08 +0300

    L - Done

--- contest/L.cpp
+++ contest/L.cpp
@@ -1,24 +1,54 @@
 #include <iostream>
-#include <deque>
-#include <algorithm>
+#include <vector>
 
-int main(){
+#define INFINITY 1000000;
 
-    std::deque<int> window;
+using namespace std;
+
+vector<int> numbers;
+vector<int> tree;
+
+void build_tree(int cur, int Lm, int Rm) {
+    if (Lm == Rm)
+        tree[cur] = numbers[Lm];
+    else {
+        build_tree(cur*2, Lm, (Lm+Rm)/2);
+        build_tree(cur*2+1, (Lm+Rm)/2 + 1, Rm);
+        tree[cur] = min(tree[2*cur], tree[2*cur + 1]);
+    }
+}
+
+
+int get_min(int cur, int Lm, int Rm, int l, int r)
+{
+    if (Lm > r || Rm < l)
+        return INFINITY;
+    if (l <= Lm && Rm <= r)
+        return tree[cur];
+
+    return min(get_min(cur*2, Lm, (Lm+Rm)/2, l, r), get_min(cur*2+1, (Lm+Rm)/2+1, Rm, l, r));
+}
+
+int main() {
+
     int N, k, t;
 
     std::cin >> N >> k;
 
-    for (int i = 0; i < k; i++)
-    {
+    numbers.reserve(N);
+    tree.assign(4 * N, 0);
+
+    for (int i = 0; i < N; i++) {
         std::cin >> t;
-        window.push_back(t);
+        numbers.push_back(t);
     }
 
-    std::sort(window.begin(), window.end());
-    std::cout << window.front();
+    build_tree(1, 0, N-1);
 
-    for (auto &el: window)
-        std::cout << el << " ";
+    for (int i = 0; i<= N-k; i++)
+    {
+        cout << get_min(1, 0, N-1, i, i+k-1) << " ";
+    }
+
 
 }
