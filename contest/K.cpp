6448fb5332fc0003ae812f909de29e90f5826c28
Arseniy <arseniypopugay@gmail.com>
2021-05-30 13:24:09 +0300

    K - Done

--- contest/K.cpp
+++ contest/K.cpp
@@ -23,6 +23,15 @@ struct CmpBlocks {
 set<struct block *, CmpBlocks> free_blocks;
 vector<struct block *> requests;
 
+void merge_chain_elem(block *a, block *b) {
+    a->size += b->size;
+    a->right = b->right;
+    if (b->right) {
+        b->right->left = a;
+    }
+    delete b;
+}
+
 int main() {
     int N, M, input;
     cin >> N >> M;
@@ -42,7 +51,7 @@ int main() {
             }
 
             auto it = free_blocks.end();
-            it --;
+            it--;
 
 
             if ((*it)->size < input) {
@@ -51,7 +60,7 @@ int main() {
                 continue;
             }
 
-            if ((*it)->size == input){
+            if ((*it)->size == input) {
                 (*it)->free = false;
                 cout << (*it)->num << endl;
                 requests.push_back(*it);
@@ -60,7 +69,11 @@ int main() {
             }
 
 
-            auto leftover = new block{(*it)->num+input, (*it)->size - input, *it, (*it)->right, true};
+            auto leftover = new block{(*it)->num + input, (*it)->size - input, *it, (*it)->right, true};
+            if ((*it)->right){
+                (*it)->right->left = leftover;
+            }
+
             (*it)->right = leftover;
             (*it)->size = input;
             (*it)->free = false;
@@ -73,129 +86,25 @@ int main() {
 
         } else {
             requests.push_back(nullptr);
-            auto el = requests[(-input)-1];
-            requests[(-input)-1] = nullptr;
+            auto el = requests[(-input) - 1];
+            requests[(-input) - 1] = nullptr;
             if (!el) continue;
             el->free = true;
 
             if (el->left && el->left->free){
-
-                auto left_elem = el->left;
-                free_blocks.erase(left_elem);
-                left_elem->size += el->size;
-                left_elem->right = el->right;
-                if (el->right)
-                    el->right->left = left_elem;
-
-                delete el;
-                el = left_elem;
+                free_blocks.erase(el->left);
+                el = el->left;
+                merge_chain_elem(el, el->right);
             }
 
-                if (el->right && el->right->free){
-                auto right_elem = el->right;
-                free_blocks.erase(right_elem);
-                el->right = right_elem->right;
-                el->size += right_elem->size;
-                if (right_elem->right)
-                    right_elem->right->left = el;
-                delete right_elem;
+            if (el->right && el->right->free){
+                free_blocks.erase(el->right);
+                merge_chain_elem(el, el->right);
             }
 
-//            cout << "Freeing " << el->num << endl;
             free_blocks.insert(el);
         }
     }
 
     return 0;
 }
-
-
-//int main(){
-//    int N, M, p;
-//    cin >> N >> M;
-//
-//    auto t = new block {1, N, nullptr, nullptr, true};
-//    free_blocks.insert(t);
-//
-//    for (int i = 0; i < M; i++)
-//    {
-//        cin >> p;
-//        if (p>0) {
-//            if (free_blocks.empty()) {
-//                requests.push_back(nullptr);
-//                cout << -1 << endl;
-//                continue;
-//            }
-//
-//            auto it = free_blocks.end();
-//            it--;
-//
-//            if ((*it)->size < p){
-//                requests.push_back(nullptr);
-//                cout << -1 << endl;
-//                continue;
-//            }
-//            else{
-//                if ((*it)->size == p)
-//                {
-//                    requests.push_back(*it);
-//                    cout << (*it)->num << endl;
-//                    (*it)->free = false;
-//                    free_blocks.erase(it);
-//                } else {
-//                    auto leftover = new block{(*it)->num+p,(*it)->size-p, *it, (*it)->right, true};
-//                    (*it)->right = leftover;
-//                    (*it)->size = p;
-//                    (*it)->free = false;
-//                    requests.push_back(*it);
-//                    cout << (*it)->num << endl;
-//                    free_blocks.erase(it);
-//                    free_blocks.insert(leftover);
-//                }
-//            }
-//
-//        } else {
-//            requests.push_back(nullptr);
-//            auto el = requests[-p-1];
-//            if (!el) continue;
-//            requests[-p-1] = nullptr;
-//            el->free = true;
-//
-//            if (el->left && el->left->free)
-//            {
-//                auto left_el = el->left;
-//                free_blocks.erase(left_el);
-//                left_el->size += el->size;
-//                left_el->right = el->right;
-//                if (el->right) {
-//                    el->right->left = left_el;
-//                }
-//                delete el;
-//                el = left_el;
-//
-//            }
-//
-//            if (el->right && el->right->free)
-//            {
-//                auto right_el = el->right;
-//                free_blocks.erase(right_el);
-//                el->size += right_el->size;
-//                el->right = right_el->right;
-//                if (el->right) {
-//                    el->right->left = el;
-//                }
-//                delete right_el;
-//
-//            }
-////            cout << "request[-p-1] " << endl;
-////            cout << "Made free block size = " << el->size << " and num = " << el->num << endl;
-////            cout << "Total free blocks = " << free_blocks.size()+1 << endl;
-//            cout << "Freeing " << el->num << endl;
-//            free_blocks.insert(el);
-////            cout << "Insertion done" << endl;
-//
-//        }
-//    }
-//
-//
-//}
